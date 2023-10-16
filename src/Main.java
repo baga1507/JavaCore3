1d771a04804c1f5dcafba0581f38e727172da640
baga1507 <baga1507@yandex.ru>
2023-10-16 18:28:45 +0300

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.lang.reflect.InvocationTargetException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+            throws InvocationTargetException, IllegalAccessException, ClassNotFoundException {
+        Testing.start(TestTaker.class);
+        System.out.println("--------------------------------------------------------------");
+        Testing.start("TestTaker");
     }
 }
